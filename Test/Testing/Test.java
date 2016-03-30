385cb8d834d295429f1456d2060afa88c2df384e
Kevin Tong <kcr1230@hotmail.com>
2016-03-30 16:37:21 -0700

    Sorts

---
+++ Test/Testing/Test.java
@@ -0,0 +1,38 @@
+public class Test {
+
+    /*
+    *
+    * public static void recursiveSelectionSort(int[] list, int start) {
+      if (start >= list.length - 1) {
+       return;
+      }
+    *
+    * */
+
+    /*
+    *
+    * int min = start;
+      for (int index = start + 1; index < list.length; index++) {
+       if (list[index] < list[min]) {
+        min = index;
+       }
+      }
+      int temp = list[start];
+      list[start] = list[min];
+      list[min] = temp;
+      recursiveSelectionSort(list, start + 1);
+    }
+    *
+    * */
+
+    /*
+      public static void main(String[] args) {
+      int[] hello = { 47, 22, 37, 5, 19};
+      recursiveSelectionSort(hello, 0);
+      for (int i = 0; i < hello.length; i++) {
+       System.out.print(hello[i] + " ");
+      }
+    }
+
+    * */
+}
