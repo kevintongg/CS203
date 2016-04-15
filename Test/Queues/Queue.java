c0a8b03a5ec9aa27a0ab5aceb03c732f4f7635ee
Kevin Tong <kcr1230@hotmail.com>
2016-04-14 18:23:42 -0700

    Lab 3

---
+++ Test/Queues/Queue.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Queue {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+        Collection<Integer> stack = new Stack<>();
+        Iterable<Integer> iterable = new HashSet<>();
+
+    }
+}
