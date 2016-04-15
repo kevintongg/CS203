4b9ca6d1b5e17d4cd42b004ba135606018ff4455
Kevin Tong <kcr1230@hotmail.com>
2016-04-15 11:31:58 -0700

    Lab

---
+++ Lab 2/src/lab2/GenericStack.java
@@ -0,0 +1,51 @@
+package lab2;
+
+import java.util.ArrayList;
+
+public class GenericStack<E> extends ArrayList<E> {
+
+    /**
+     * Push a new element into the top of the stack
+     */
+
+    public void push(E value) {
+        add(value);
+    }
+
+    /**
+     * Return and remove the top element from the stack
+     */
+
+    public E pop() {
+        return remove(getSize() - 1);
+    }
+
+
+    /**
+     * Return the top element from the stack
+     */
+
+    public E peek() {
+
+        return get(getSize() - 1);
+
+    }
+
+    /**
+     * Exercise03_21 whether the stack is empty
+     */
+
+    public boolean isEmpty() {
+
+        return super.isEmpty();
+
+    }
+
+    /**
+     * Return the number of elements in the stack
+     */
+
+    public int getSize() {
+        return size();
+    }
+}
---
+++ Lab 3/src/lab3/Lab3.java
@@ -0,0 +1,53 @@
+package lab3;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lab3 {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        readFromInsertedOrder();
+        readNonDuplicatesAscending();
+
+    }
+
+    private static void readFromInsertedOrder() {
+
+        List<String> linkedList = new LinkedList<>();
+
+        int counter = 0;
+        System.out.println("We will now get five words to display");
+        do {
+            System.out.println("Please enter word #" + (counter + 1) + ":");
+            linkedList.add(sc.next());
+            counter++;
+        } while (counter != 5);
+
+        linkedList.forEach(System.out::println);
+
+    }
+
+    private static void readNonDuplicatesAscending() {
+
+        List<String> arrayList = new ArrayList<>();
+
+        int counter = 0;
+        System.out.println("We will now get five words to display in ascending order with duplicates removed");
+        do {
+            System.out.println("Please enter word #" + (counter + 1) + ":");
+            arrayList.add(sc.next());
+            counter++;
+        } while (counter != 5);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.contains())
+        }
+
+        arrayList.forEach(System.out::println);
+    }
+}
